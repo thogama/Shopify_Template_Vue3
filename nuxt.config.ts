040c7035bf98899f12883d0943bb4e8f675673a5
thogama <dethfiregrasp@gmail.com>
2022-08-07 15:02:31 -0300

    static css and ssr:off

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,22 @@
 import { defineNuxtConfig } from 'nuxt'
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     head: [
-        {
-            rel: "stylesheet",
-            href: "/css/bootstrap.min.css"
-        }
+        
+        
+
     ],
-    css:[
-        "~~/static/css/bootstrap.min.css"
+    ssr:false,
+    css: [
+        "~~/static/css/bootstrap.min.css",
+        "@fortawesome/fontawesome-svg-core/styles.css",
+        "~~/sass/main.scss"
     ],
     
-    
+
+
+
+
 })
 
 
