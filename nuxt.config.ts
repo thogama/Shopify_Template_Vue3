bc8428f976dc21942af1c83e04b112bc773bcfc9
thogama <dethfiregrasp@gmail.com>
2022-08-16 11:58:01 -0300

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,16 @@
 import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    
     ssr:false,
     css: [
         
         "@fortawesome/fontawesome-svg-core/styles.css",
         "~~/sass/main.scss"
     ],
+    nitro:{
+        preset:"node-server"
+    }
 })
 
 
