e93d9530675819f839c6b8b9b129f6b6529c78a6
thogama <dethfiregrasp@gmail.com>
2022-08-16 11:45:49 -0300

    remove start

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,8 @@
 import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    
+
     ssr:false,
-    target:'static',
     css: [
         
         "@fortawesome/fontawesome-svg-core/styles.css",
