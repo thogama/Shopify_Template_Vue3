5e502294f7db4619a85f2dc6bd9b151d2fa28d99
thogama <dethfiregrasp@gmail.com>
2022-08-16 19:55:05 -0300

    nuxt rc8

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,50 +1,13 @@
 import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    
     ssr: false,
     css: [
 
         "@fortawesome/fontawesome-svg-core/styles.css",
         "~~/sass/main.scss"
     ],
-
-    // Global page headers: https://go.nuxtjs.dev/config-head
-    head: {
-        title: 'heroku',
-        htmlAttrs: {
-            lang: 'en'
-        },
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: '' },
-            { name: 'format-detection', content: 'telephone=no' }
-        ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-        ]
-    },
-
-
-    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-    plugins: [
-    ],
-
-    // Auto import components: https://go.nuxtjs.dev/config-components
-    components: true,
-
-    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-    buildModules: [
-    ],
-
-    // Modules: https://go.nuxtjs.dev/config-modules
-    modules: [
-    ],
-
-    // Build Configuration: https://go.nuxtjs.dev/config-build
-    build: {
-    }
 
 
 
