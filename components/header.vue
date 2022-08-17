3c85d790969ded11cc28bf03560c73a471564ca2
thogama <dethfiregrasp@gmail.com>
2022-08-17 14:57:18 -0300

    new header with hide on scroll

--- components/header.vue
+++ components/header.vue
@@ -1,45 +1,55 @@
 <template>
-    <div style="position: absolute;width: 100%;" class="accordion" id="accordionFlushExample">
+    <header style="position: fixed;width: 100%;top:0vh ;z-index: 1;" class="accordion" id="accordionFlushExample">
         <div class="accordion-item">
 
             <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne"
                 data-bs-parent="#accordionFlushExample">
-                <div class="row border p-4 accordion-body">
-                    <div class="col-3">
+                <div class="row  border p-4 accordion-body">
+                    <div class="col-1"></div>
+                    <div style="background-color: whitesmoke;" class="d-flex align-items-center p-0 col">
 
-                    </div>
-                    <input placeholder="Search for Products" class="col border border-0 form-control" type="text" />
-                    <div class="col-2">
+                        <font-awesome-icon class="ms-1 me-2 col-1 p-3 fa-md ps-0 ms-0 pe-0 me-0 "
+                            icon="fa-solid fa-magnifying-glass" />
 
+                        <input placeholder="Search for Products" class=" ps-0 ms-0 col border  form-control"
+                            type="text" />
                     </div>
                     <font-awesome-icon data-bs-toggle="collapse" data-bs-target="#flush-collapseOne"
                         aria-expanded="false" aria-controls="flush-collapseOne" id="busca"
-                        class="vue-svg  col-1 p-2 fa-2xl" icon="fa-solid fa-xmark" />
+                        class=" vue-svg col-1 p-3 fa-lg" icon="fa-solid fa-xmark" />
+
                 </div>
+                <div class="row text-center accordion-body">
+                    <div class="col-6">666</div>
+                    <div class="col-6">66</div>
+                </div>
+
             </div>
+            <hr />
         </div>
 
 
-    </div>
+
+    </header>
 
 
 
-    <div  class=" p-3 border-bottom" id="header-hover">
+    <div class=" p-3 border-bottom" id="header-hover">
 
-        <div id="example-1" class=" align-items-center row">
+        <div id="example-1" class=" align-items-center d-flex  justify-content-around">
 
 
 
-            <font-awesome-icon data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false"
-                aria-controls="flush-collapseOne" id="busca" class="col p-1 vue-svg"
+            <font-awesome-icon style="width: 10vh" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne"
+                aria-expanded="false" aria-controls="flush-collapseOne" id="busca" class="  vue-svg"
                 icon="fa-solid fa-magnifying-glass" />
 
 
-            <font-awesome-icon class=" vue-svg primary col p-2 fa-2xl" icon="fa-brands fa-vuejs" />
+            <font-awesome-icon style="width: 10vh" class=" vue-svg primary  p-2  fa-2xl" icon="fa-brands fa-vuejs" />
 
 
 
-            <font-awesome-icon class="vue-svg col  " icon="fa-solid fa-bars" />
+            <font-awesome-icon style="width: 10vh;" class="vue-svg   " icon="fa-solid fa-bars" />
 
 
 
@@ -49,14 +59,24 @@
 
 <script>
 
-
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    const hold = document.getElementById("header-hover").style.top
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("header-hover").style.top = "0";
+    } else {
+        document.getElementById("header-hover").style.top = "-100px";
+    }
+    prevScrollpos = currentScrollPos;
+}
 
 </script>
 
 <style scoped>
 .collapse {
-    transition: height  0.45s ease;
-    height: 85vh;
+    transition: height 0.45s ease;
+    height: 100vh;
     background-color: whitesmoke;
 
 
@@ -65,13 +85,18 @@
 
 .collapsing {
     transition: height 0.5s ease;
-    background-color: whitesmoke;
+    background-color: white;
 
 }
 
 #header-hover {
+    position: fixed;
     background-color: black;
-
+    top: 0;
+    width: 100%;
+    display: block;
+    transition: top 0.3s;
+    z-index: 0;
 }
 
 #header-hover:hover {
@@ -99,5 +124,10 @@
 .vue-svg:hover {
 
     color: black;
+}
+
+/*asdasd */
+section>p {
+    color: #000;
 }
 </style>
