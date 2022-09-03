bf7895ae82b776fecb3d6623e75aa34815c3ae3f
thogama <dethfiregrasp@gmail.com>
2022-09-03 08:00:02 -0300

    using lorem ipsulum

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,70 +1,36 @@
 <template>
     <div class="text-break">
-        node_modules\bootstrap\scss\_functions.scss 235:43 add()
-        node_modules\bootstrap\scss\_variables.scss 852:41 @import
-        node_modules\bootstrap\scss\bootstrap.scss 8:9 @import
-        sass\main.scss 14:9 root stylesheet
-        at Object.wrapException (C:\Users\Alan Gama\Desktop\Vue\node_modules\sass\sass.dart.js:1246:17)
-        at _SerializeVisitor0.visitNumber$1 (C:\Users\Alan Gama\Desktop\Vue\node_modules\sass\sass.dart.js:92283:19)
-        at ComplexSassNumber0.accept$1$1 (C:\Users\Alan Gama\Desktop\Vue\node_modules\sass\sass.dart.js:89058:22)
-        at ComplexSassNumber0.accept$1 (C:\Users\Alan Gama\Desktop\Vue\node_modules\sass\sass.dart.js:89061:19)
-        at Object.serializeValue0 (C:\Users\Alan Gama\Desktop\Vue\node_modules\sass\sass.dart.js:21884:13)
-        at _EvaluateVisitor__serialize_closure2.call$0 (C:\Users\Alan
-        Gama\Desktop\Vue\node_modules\sass\sass.dart.js:76674:16)
-        at _EvaluateVisitor2._async_evaluate0$_addExceptionSpan$1$2 (C:\Users\Alan
-        Gama\Desktop\Vue\node_modules\sass\sass.dart.js:73376:23)
-        at _EvaluateVisitor2._async_evaluate0$_addExceptionSpan$2 (C:\Users\Alan
-        Gama\Desktop\Vue\node_modules\sass\sass.dart.js:73397:19)
-        at _EvaluateVisitor2._async_evaluate0$_serialize$3$quote (C:\Users\Alan
-        Gama\Desktop\Vue\node_modules\sass\sass.dart.js:73112:19)
-        at C:\Users\Alan Gama\Desktop\Vue\node_modules\sass\sass.dart.js:76659:39
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tincidunt euismod ex sed vehicula.
+        Suspendisse finibus, eros a scelerisque cursus, orci mauris aliquam magna, viverra efficitur augue neque a
+        purus. Duis porttitor eros arcu. Nullam quis pulvinar mauris. Etiam non euismod quam. Aliquam euismod arcu et
+        odio aliquam congue. Aenean bibendum dignissim neque. Nunc congue risus id massa scelerisque scelerisque. Mauris
+        vulputate arcu non eros lacinia rhoncus. Maecenas condimentum porta purus, eget mattis tellus interdum ut. Ut
+        finibus sapien id rhoncus sollicitudin.
 
+        Nulla molestie neque et neque porta, a ornare eros efficitur. Cras fringilla semper ligula. Maecenas pulvinar
+        mauris magna, sed dignissim mauris dapibus in. Phasellus suscipit luctus massa non vulputate. Nunc dictum
+        blandit diam, ac auctor nunc. Nunc a commodo velit. Phasellus rhoncus ultricies libero, non dapibus justo
+        posuere a. Phasellus ullamcorper ornare metus, molestie cursus mi luctus vel. Donec eget nunc dui. Mauris congue
+        lorem in sollicitudin fermentum. Cras volutpat sem ut iaculis auctor.
 
-        ERROR 5px*em isn't a valid CSS value. 01:39:55
-        ╷
-        235 │ @return if($return-calc == true, calc(#{$value1} + #{$value2}), $value1 + unquote(" + ") + $value2);
-        │ ^^^^^^^
-        ╵
-        node_modules\bootstrap\scss\_functions.scss 235:43 add()
-        node_modules\bootstrap\scss\_variables.scss 852:41 @import
-        node_modules\bootstrap\scss\bootstrap.scss 8:9 @import
-        sass\main.scss 14:9 root stylesheet
+        Nullam sit amet ornare mi, et porttitor massa. Nulla velit odio, consectetur quis facilisis quis, interdum eu
+        risus. Sed a justo quam. Suspendisse porttitor urna et ante laoreet facilisis. Sed suscipit ornare nulla,
+        venenatis elementum libero vehicula ac. Nulla in varius tortor. Integer malesuada at purus vel lobortis. Sed
+        iaculis, lorem nec pellentesque placerat, tortor lorem vehicula est, auctor auctor ipsum augue nec sapien. Cras
+        eu lectus vitae libero accumsan vestibulum vitae nec orci. Integer venenatis, magna sit amet lobortis pretium,
+        neque lacus pellentesque neque, sed faucibus neque tortor vitae dolor. Praesent lacinia mi id posuere iaculis.
+        Pellentesque ultrices est nibh, eu tempor ante sollicitudin sit amet. Etiam tincidunt diam nec tortor
+        ullamcorper, sit amet vehicula ex tristique.
 
+        Praesent porttitor tincidunt ipsum sit amet malesuada. Etiam commodo ante ac rutrum elementum. Nullam interdum
+        tellus sed mattis scelerisque. Cras congue dui fermentum congue aliquet. Sed vitae interdum leo, eu blandit sem.
+        Quisque dignissim eget metus tincidunt semper. Ut elementum tristique odio sed blandit.
 
-        ERROR 01:39:55 [vite] Internal server error: 5px*em isn't a valid CSS value. 01:39:55
-        ╷
-        235 │ @return if($return-calc == true, calc(#{$value1} + #{$value2}), $value1 + unquote(" + ") + $value2);
-        │ ^^^^^^^
-        ╵
-        node_modules\bootstrap\scss\_functions.scss 235:43 add()
-        node_modules\bootstrap\scss\_variables.scss 852:41 @import
-        node_modules\bootstrap\scss\bootstrap.scss 8:9 @import
-        sass\main.scss 14:9 root stylesheet
-        Plugin: vite:css
-        File: C:\Users\Alan Gama\Desktop\Vue\node_modules\bootstrap\scss\_functions.scss
-        Error: 5px*em isn't a valid CSS value.
-        ╷
-        235 │ @return if($return-calc == true, calc(#{$value1} + #{$value2}), $value1 + unquote(" + ") + $value2);
-        │ ^^^^^^^
-        ╵
-        node_modules\bootstrap\scss\_functions.scss 235:43 add()
-        node_modules\bootstrap\scss\_variables.scss 852:41 @import
-        node_modules\bootstrap\scss\bootstrap.scss 8:9 @import
-        sass\main.scss 14:9 root stylesheet
-        at Object.wrapException (C:\Users\Alan Gama\Desktop\Vue\node_modules\sass\sass.dart.js:1246:17)
-        at _SerializeVisitor0.visitNumber$1 (C:\Users\Alan Gama\Desktop\Vue\node_modules\sass\sass.dart.js:92283:19)
-        at ComplexSassNumber0.accept$1$1 (C:\Users\Alan Gama\Desktop\Vue\node_modules\sass\sass.dart.js:89058:22)
-        at ComplexSassNumber0.accept$1 (C:\Users\Alan Gama\Desktop\Vue\node_modules\sass\sass.dart.js:89061:19)
-        at Object.serializeValue0 (C:\Users\Alan Gama\Desktop\Vue\node_modules\sass\sass.dart.js:21884:13)
-        at _EvaluateVisitor__serialize_closure2.call$0 (C:\Users\Alan
-        Gama\Desktop\Vue\node_modules\sass\sass.dart.js:76674:16)
-        at _EvaluateVisitor2._async_evaluate0$_addExceptionSpan$1$2 (C:\Users\Alan
-        Gama\Desktop\Vue\node_modules\sass\sass.dart.js:73376:23)
-        at _EvaluateVisitor2._async_evaluate0$_addExceptionSpan$2 (C:\Users\Alan
-        Gama\Desktop\Vue\node_modules\sass\sass.dart.js:73397:19)
-        at _EvaluateVisitor2._async_evaluate0$_serialize$3$quote (C:\Users\Alan
-        Gama\Desktop\Vue\node_modules\sass\sass.dart.js:73112:19)
-        at C:\Users\Alan Gama\Desktop\Vue\node_modules\sass\sass.dart.js:76659:39
+        Nam ac ante at arcu commodo tempor. Nullam feugiat metus eget blandit efficitur. Fusce sed mi non felis
+        dignissim tincidunt ut id dolor. Suspendisse gravida varius ipsum eget finibus. Phasellus nulla lorem,
+        vestibulum et turpis id, dictum fringilla justo. Duis ex velit, rhoncus eu tellus quis, elementum porttitor
+        velit. Quisque quis dictum ipsum. Pellentesque aliquet pellentesque enim porta vestibulum. Etiam eleifend tortor
+        et rutrum cursus. Interdum et malesuada fames ac ante ipsum primis in faucibus.
     </div>
 </template>
 
--- pages/product/[id].vue
+++ pages/product/[id].vue
@@ -3,14 +3,14 @@ const route = useRoute();
 
 </script>
 <template>
-    <div class="p-2 row">
+    <div class="p-2 row gy-3">
         <h1> Produto {{ route.params.id }}</h1>
 
-        <div class="col-6">
+        <div class="col-sm-6">
             <img class="img-fluid" src="~assets/blue.jpg" alt="guitar image">
 
         </div>
-        <div class="col-6">
+        <div class="col-sm-6    ">
             <img class="img-fluid" src="~assets/blue.jpg" alt="guitar image">
 
         </div>
