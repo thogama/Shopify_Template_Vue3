65d782f0cee7aeda8d21bcc1781e3efca500b123
thogama <dethfiregrasp@gmail.com>
2022-09-03 07:59:33 -0300

    New design

--- components/sidebar.vue
+++ components/sidebar.vue
@@ -3,14 +3,24 @@
 
 
   <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
-    <div style="background-color: black;" class="text-primary offcanvas-header">
-      <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
-      <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    <div class="bg-dark text-white offcanvas-header">
+      <h5 class=" offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+      <button type="button" class="btn-primary btn bg-primary btn-close text-reset" data-bs-dismiss="offcanvas"
+        aria-label="Close"></button>
     </div>
-    <div class="d-flex p-5 flex-column align-items-center offcanvas-body">
-      <h1 class="p-2">Login</h1>
-      <Login class="mt-5"  />
-      
+    <div style="height:100vh;background-image: repeating-linear-gradient(45deg, #d79a00,#e6bd55,#f0d798, transparent 100px);"
+      class="d-flex p-5 flex-column align-items-center offcanvas-body">
+      <div class="shadow-lg p-3">
+        <h1 class="p-2">Login
+
+        </h1>
+
+
+
+        <Login class="mt-5" />
+      </div>
+
+
     </div>
   </div>
 </template>
