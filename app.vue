f510b6a0ca87a2100b20c7abda9621660dffa769
thogama <dethfiregrasp@gmail.com>
2022-09-03 08:01:10 -0300

    Starting pages transitions

--- app.vue
+++ app.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
     <NuxtLayout>
+      <NuxtLoadingIndicator />
       <NuxtPage class=" m-0" />
     </NuxtLayout>
   </div>
 </template>
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@400;600&display=swap');
+
 html {
   overflow-x: hidden;
 }
