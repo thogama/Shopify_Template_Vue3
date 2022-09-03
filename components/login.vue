e4917473960a072d1a2b4fb939957e2e73546646
thogama <dethfiregrasp@gmail.com>
2022-09-03 07:58:12 -0300

    Mensages

--- components/login.vue
+++ components/login.vue
@@ -2,31 +2,40 @@
     <form @submit="(e) => do_search(email, password, e)">
         <div class="col-12">
 
-            <div class="bg-input-group p-1 d-flex align-items-center ">
+            <div class="rounded-pill bg-input-group p-1 d-flex align-items-center ">
                 <font-awesome-icon class="p-2 iconed" icon="fa-solid fa-user" />
                 <div class="vr m-1"></div>
                 <input v-model="state.email" type="email" class="form-control" placeholder="name@example.com">
 
             </div>
             <p class=" p-1 text-danger" v-if="v$.email.$error">
-                {{  v$.email.$errors[0].$message  }}
+                {{ v$.email.$errors[0].$message }}
             </p>
 
-            <div class="bg-input-group p-1 d-flex align-items-center ">
+            <div class="rounded-pill bg-input-group p-1 d-flex align-items-center ">
                 <font-awesome-icon class="p-2 iconed" icon="fa-solid fa-key" />
                 <div class="vr m-1"></div>
                 <input v-model="state.password" type="password" class="form-control" placeholder="Password">
             </div>
             <p class="p-1  text-danger" v-if="v$.password.$error">
-                {{  v$.password.$errors[0].$message  }}
+                {{ v$.password.$errors[0].$message }}
             </p>
-            <div class="row justify-content-center">
+            <div class=" row aligns-items-center justify-content-center">
+                <div class="mt-3 d-flex align-items-center">
+                    <div class="p-0 bg-transparent input-group-text">
+                        <input class="m-1 bg-dark form-check-input mt-0" type="checkbox" value=""
+                            aria-label="Checkbox for following text input">
+                        <p class="m-0">Remenber Me</p>
+                    </div>
+
+                </div>
                 <button @click="(e) => submitForm(e)" type="submit"
-                    class="rounded-pill mt-5 p-3 col-10 btn btn-primary">
+                    class="text-white rounded-pill mt-5 p-3 col-10 btn btn-dark">
                     Login
                 </button>
+
                 <button style="border: 1px solid red;" type="submit"
-                    class="text-danger rounded-pill mt-4   p-3 col-10 btn btn-outline-danger">
+                    class=" text-white  rounded-pill mt-4   p-3 col-10 btn btn-danger ">
                     Google
                 </button>
             </div>
@@ -40,15 +49,6 @@
 import { useVuelidate } from '@vuelidate/core'
 import { required, email, minLength, helpers } from '@vuelidate/validators'
 
-
-
-
-
-
-
-const do_search = async (email: string, password: string, e: Event) => {
-    e.preventDefault()
-}
 
 export default {
     setup() {
@@ -81,7 +81,7 @@ export default {
             if (!this.v$.$error) {
                 alert("submit")
             }
-            
+
         }
     }
 
